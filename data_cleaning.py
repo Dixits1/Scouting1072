2be18211b1a8bde4ebc97b8bcc4513d1b0f99b0b
Arjun Dixit <technoarjun@gmail.com>
2022-03-25 11:39:40 -0700

    updated csv

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,7 +2,43 @@ import csv
 import pandas as pd
 
 # read in qr_backup.csv as txt file
+f = open('qr_backup.csv', 'r')
 
+# split file into lines
+lines = f.readlines()
+
+new_lines = []
+
+for line in lines:
+    # split line into pieces by commas
+    pieces = line.split(',')
+    new_pieces = []
+
+    # for the first 13 elements of pieces, store in new_pieces. for all elements in pieces after the 13th element, 
+    # combine into 1 string and store a the end of new_pieces
+    for i in range(13):
+        new_pieces.append(pieces[i])
+
+    last_elem = ' '.join(pieces[13:])
+
+    # in the last_elem string, iterate over each character. if character is not a letter from A-Z (either uppercase or lowercase) and is not a number, then replace it with a space
+    for i in range(len(last_elem)):
+        if not (last_elem[i].isalpha() or last_elem[i].isdigit() or last_elem[i] == '(' or last_elem == ')'):
+            last_elem = last_elem[:i] + ' ' + last_elem[i+1:]
+
+
+    # combine all elements in pieces after the 13th element into 1 string
+    new_pieces.append(last_elem)
+
+    pieces_merged = ','.join(new_pieces)
+
+    # add new_pieces to new_lines
+    new_lines.append(pieces_merged)
+
+# compile new_lines into a string seperated by new lines
+new_lines_str = '\n'.join(new_lines)
+
+print(new_lines_str)
 
 # read in qr.csv
 df = pd.read_csv('qr.csv')
